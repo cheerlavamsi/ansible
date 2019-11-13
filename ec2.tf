cbc6c15f57b606a09a6911fb597cef345cd5f30e
chvamsidevops <chvamsi.devops@gmail.com>
2019-11-13 08:43:08 +0530

    ec2.tf added

---
+++ ec2.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "web" {
+  count         = 2
+  ami           = "ami-02383135f96b2293e"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ansible-node-${count.index+1}"
+  }
+}
+###
