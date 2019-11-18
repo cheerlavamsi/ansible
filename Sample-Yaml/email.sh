0fa05ca39f13cf0e0f0becd118b7350e24ec4ac7
prasad balannagaru <chvamsi.devops@gmail.com>
2019-11-18 08:55:01 +0530

    Added SH

--- Sample-Yaml/email.sh
+++ Sample-Yaml/email.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 read -p 'Enter batch no: ' batch_no
+cat courses.yml | yq . | jq '.[].batchno' | grep -w $batch_no &>/dev/null
+if [ $? -ne 0 ]; then
+    echo "Given batch no doesnot exist"
+    exit 1
+fi
