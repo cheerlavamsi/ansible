9bfa44c126dd13dfb820b89f20712c32cf8b59d4
prasad balannagaru <chvamsi.devops@gmail.com>
2019-11-18 14:22:02 +0530

    aaa

--- terraform/01/ec2.tf
+++ terraform/01/ec2.tf
@@ -2,8 +2,7 @@ resource "aws_instance" "web" {
   count         = 2
   ami           = "ami-02383135f96b2293e"
   instance_type = "t2.micro"
-  
-  tags = { 
+    tags = { 
     Name = "ansible-node-${count.index+1}"
   }
 }
