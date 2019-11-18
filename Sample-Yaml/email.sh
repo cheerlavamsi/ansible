d89196db489a78c5e78637ce04be5c20d3191311
prasad balannagaru <chvamsi.devops@gmail.com>
2019-11-18 16:26:39 +0530

    Added emails

--- Sample-Yaml/email.sh
+++ Sample-Yaml/email.sh
@@ -6,3 +6,13 @@ if [ "$VALUE" = "null" ]; then
     echo "Given batch no doesnot exist"
     exit 1
 fi
+
+NAMES=('cat courses.yml |yq . | jq '.b42.students[].name'|xargs')
+EMAILS=('cat courses.yml | yq .| jq '.b42.students[].email'|xargs')
+
+VALUE_NO=$(echo ${#NAMES[*]})
+
+while [ $VALUE_NO -gt 0]; do 
+    VALUE_NO=$(($VALUE_NO-1))
+    echo -e "Hello ${NAMES[$VALUE_NO]}, I am sending an emails to ${EMAILS[VALUE_NO]}"
+done
