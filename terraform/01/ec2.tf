834d96b4e89dc8127811964980ec8145126c4d2f
prasad balannagaru <chvamsi.devops@gmail.com>
2019-11-18 09:17:50 +0530

    a

--- terraform/01/ec2.tf
+++ terraform/01/ec2.tf
@@ -2,8 +2,8 @@ resource "aws_instance" "web" {
   count         = 2
   ami           = "ami-02383135f96b2293e"
   instance_type = "t2.micro"
-  vpc_security_group_ids= "sg-098fd4a5ffc0e66b6"
-  key_name          = "devops"
+  ## vpc_security_group_ids= "sg-098fd4a5ffc0e66b6"
+  ## key_name          = "devops"
   
   tags = {
     Name = "ansible-node-${count.index+1}"
