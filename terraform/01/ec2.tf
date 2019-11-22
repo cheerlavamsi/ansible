c0c890329f79c07e507f6507ad4ee37be4a22903
prasad balannagaru <chvamsi.devops@gmail.com>
2019-11-22 19:35:33 +0530

    Added Securirty Groupo

--- terraform/01/ec2.tf
+++ terraform/01/ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "web" {
   ami           = "ami-02383135f96b2293e"
   instance_type = "t2.micro"
 
-  vpc_security_group_ids = ["sg-098fd4a5ffc0e66b6"]
+  vpc_security_group_ids = ["sg-098fd4a5ffc0e66b6" ,"sg-09734e0ae6147a09b"]
   key_name  = "Devops"
 
   tags = { 
