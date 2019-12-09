9a5bd71b831dd3357b6f4b0feb70f408434e2d37
prasad balannagaru <chvamsi.devops@gmail.com>
2019-12-09 12:00:47 +0530

    add

--- Projects/Studentapp/roles/cloudwatch/templates/cloud-watch-metrics.sh
+++ Projects/Studentapp/roles/cloudwatch/templates/cloud-watch-metrics.sh
@@ -7,6 +7,6 @@ JAVA_PROC_COUNT=$(ps -ef | grep java | grep -v grep | wc -l)
 AWS="aws --region {{AWS_REGION}}"
 
 $AWS cloudwatch put-metric-data --metric-name Web-Process --namespace 
-System-Process --timestamp --value $WEB_PROC_COUNT --unit Milliseconds
+System-Process --timestamp $DATE --value $WEB_PROC_COUNT --unit Milliseconds
 $AWS cloudwatch put-metric-data --metric-name Java-Process --namespace 
-System-Process --timestamp  --value $JAVA_PROC_COUNT --unit Milliseconds
+System-Process --timestamp $DATE --value $JAVA_PROC_COUNT --unit Milliseconds
