561141c42cb003e962300565d7c08f8bd5328e29
cheerlavamsi <chvamsi.devops@gmail.com>
2020-04-27 14:33:23 +0530

    add

--- terraform/01/ec2.tf
+++ terraform/01/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" {
   count         = 2
-  ami           = "ami-02383135f96b2293e"
+  ami           = "ami-0ae75460021c23d13"
   instance_type = "t2.micro"
 
   vpc_security_group_ids = ["sg-098fd4a5ffc0e66b6" ,"sg-09734e0ae6147a09b"]
